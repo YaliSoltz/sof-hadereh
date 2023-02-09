0cd6bc6884df9a22c52211ea9967549d2d0dda3f
YaliSoltz <yali.soltz11@gmail.com>
2023-02-09 20:47:26 +0200

    redesign contact-us from and adding a credit text

--- client/src/comp-user/contactUs.jsx
+++ client/src/comp-user/contactUs.jsx
@@ -5,26 +5,27 @@ import { ContactUsContext } from "../context/contactUs";
 import Validation from "./validation";
 
 const ContactUs = () => {
-  const { contactUs, addNewContactUs, deleteContactUs } =
-    useContext(ContactUsContext);
+  const { addNewContactUs } = useContext(ContactUsContext);
 
-  const [newContactUs, setNewContactUs] = useState({
-    // new ContactUs object
+  const [newContactUs, setNewContactUs] = useState({ // new ContactUs object
     name: "",
     phoneNumber: "",
     email: "",
     subject: "",
     content: "",
   });
+
   const [errors, setErrors] = useState({});
 
   // function that add new ContactUs and reset the form
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrors(Validation(newContactUs));
-    addNewContactUs(newContactUs); // add newContactUs to database
-    console.log(newContactUs);
+    if(Object.keys(errors).length>0) return
+    document.getElementsByName("name")[0].style.border = "2px solid red";
+    // addNewContactUs(newContactUs); // add newContactUs to database
     document.getElementById("form").reset(); // reset the form
+    console.log(newContactUs);
   };
 
   // useEffect(() => {
@@ -42,87 +43,94 @@ const ContactUs = () => {
           handleSubmit(e);
         }}
       >
-        {errors.name && <span className="error"> {errors.name}</span>}
+        <div className="input-wrapper">
+          <input
+            type="text"
+            className="field"
+            placeholder="שם:"
+            // required
+            name="name"
+            value={newContactUs.name}
+            onChange={(e) =>
+              setNewContactUs({ ...newContactUs, name: e.target.value })
+            }
+          />
+          {errors.name && <span className="error"> {errors.name}</span>}
+        </div>
 
-        <input
-          type="text"
-          className="field"
-          placeholder="שם:"
-          // required
-          name="name"
-          value={newContactUs.name}
-          onChange={(e) =>
-            setNewContactUs({ ...newContactUs, name: e.target.value })
-          }
-        />
+        <div className="input-wrapper">
+          <input
+            id="phone"
+            type="number"
+            className="field"
+            placeholder="טלפון:"
+            minLength={10}
+            // required
+            name="phoneNumber"
+            value={newContactUs.phoneNumber}
+            onChange={(e) =>
+              setNewContactUs({
+                ...newContactUs,
+                phoneNumber: e.target.value,
+              })
+            }
+          />
+          {errors.phoneNumber && (
+            <span className="error"> {errors.phoneNumber}</span>
+          )}
+        </div>
 
-        {errors.phoneNumber && (
-          <span className="error"> {errors.phoneNumber}</span>
-        )}
+        <div className="input-wrapper">
+          <input
+            type="email"
+            className="field"
+            placeholder="אימייל:"
+            // required
+            name="email"
+            value={newContactUs.email}
+            onChange={(e) =>
+              setNewContactUs({ ...newContactUs, email: e.target.value })
+            }
+          />
+          {errors.email && <span className="error"> {errors.email}</span>}
+        </div>
 
-        <input
-          id="phone"
-          type="number"
-          className="field"
-          placeholder="טלפון:"
-          minLength={10}
-          // required
-          name="phoneNumber"
-          value={newContactUs.phoneNumber}
-          onChange={(e) =>
-            setNewContactUs({
-              ...newContactUs,
-              phoneNumber: e.target.value,
-            })
-          }
-        />
+        <div className="input-wrapper">
+          <input
+            type="text"
+            className="field"
+            placeholder="נושא:"
+            // required
+            name="subject"
+            value={newContactUs.subject}
+            onChange={(e) =>
+              setNewContactUs({ ...newContactUs, subject: e.target.value })
+            }
+          />
+          {errors.subject && <span className="error"> {errors.subject}</span>}
+        </div>
 
-        {errors.email && <span className="error"> {errors.email}</span>}
-
-        <input
-          type="email"
-          className="field"
-          placeholder="אימייל:"
-          // required
-          name="email"
-          value={newContactUs.email}
-          onChange={(e) =>
-            setNewContactUs({ ...newContactUs, email: e.target.value })
-          }
-        />
-
-        {errors.subject && <span className="error"> {errors.subject}</span>}
-
-        <input
-          type="text"
-          className="field"
-          placeholder="נושא:"
-          // required
-          name="subject"
-          value={newContactUs.subject}
-          onChange={(e) =>
-            setNewContactUs({ ...newContactUs, subject: e.target.value })
-          }
-        />
-
-        {errors.content && <span className="error"> {errors.content}</span>}
-
-        <input
-          type="text"
-          className="field"
-          placeholder="תוכן:"
-          // required
-          name="content"
-          value={newContactUs.content}
-          onChange={(e) =>
-            setNewContactUs({ ...newContactUs, content: e.target.value })
-          }
-        />
-
-        <button className="add" type="submit">
+        <div className="input-wrapper">
+          <input
+            type="text"
+            className="field"
+            placeholder="תוכן:"
+            // required
+            name="content"
+            value={newContactUs.content}
+            onChange={(e) =>
+              setNewContactUs({ ...newContactUs, content: e.target.value })
+            }
+          />
+          {errors.content && <span className="error"> {errors.content}</span>}
+        </div>
+        <button className="send" type="submit">
           שליחת הודעה
         </button>
       </form>
+    
+        <p className="credit-text">אפיון, עיצוב ובניה ע"י דן וולפר ויה-לי סולץ</p>
+        
     </div>
   );
 };
