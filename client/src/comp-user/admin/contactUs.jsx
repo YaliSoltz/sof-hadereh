4fd0251239e62ef3fe5909ff6b889aaaf9386046
YaliSoltz <yali.soltz11@gmail.com>
2023-02-11 17:26:37 +0200

    changed the table and delete bootstrap

--- client/src/comp-user/admin/contactUs.jsx
+++ client/src/comp-user/admin/contactUs.jsx
@@ -1,48 +1,54 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useContext } from "react";
-import { ContactUsContext } from './../../context/contactUs';
-import "../../css/Admin.css"
+import { ContactUsContext } from "./../../context/contactUs";
 
-const ContactUs=()=>{
-const {contactUs,deleteContactUs} = useContext(ContactUsContext);
- const [refresh,setRefresh]=useState(false)
+const ContactUs = () => {
+  const { contactUs, deleteContactUs } = useContext(ContactUsContext);
+  const [refresh, setRefresh] = useState(false);
 
-useEffect(() => {
-   if (refresh){
-    window.location.reload()
-   }
+  useEffect(() => {
+    if (refresh) {
+      window.location.reload();
+    }
   }, [refresh]);
 
-return(
-    <div className="main">
-<table className="table" >
-<thead>
-    <tr>
-        <th>שם:</th>
-        <th>טלפון:</th>
-        <th>אימייל:</th>
-        <th>נושא:</th>
-        <th>תוכן:</th>
-    </tr>
-</thead>
-<tbody>
-  
-    {contactUs.map((contact,index)=>(
-          <tr key={index} >
-        <td  >{contact.name}
-        </td>
-        <td >{contact.phoneNumber}</td>
-        <td >{contact.email}</td>
-        <td >{contact.subject}</td>
-        <td>{contact.content} <button  className="deletecontactUs" onClick={()=>(deleteContactUs(contact._id),setRefresh(true))}>מחיקה </button></td>
-        </tr>
-))}
-    
-</tbody>
-</table>
-</div>
-);
+  return (
+    <div>
+      <table className="contactUs-table">
+        <thead>
+          <tr>
+            <th>שם:</th>
+            <th>טלפון:</th>
+            <th>אימייל:</th>
+            <th>נושא:</th>
+            <th>תוכן:</th>
+            <th>מחיקה</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactUs.map((contact, index) => (
+            <tr key={index}>
+              <td>{contact.name}</td>
+              <td>{contact.phoneNumber}</td>
+              <td>{contact.email}</td>
+              <td>{contact.subject}</td>
+              <td>{contact.content}</td>
+              <td>
+                <button
+                  className="delete-contactUs"
+                  onClick={() => (
+                    deleteContactUs(contact._id), setRefresh(true)
+                  )}
+                >
+                  מחיקה
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
-export default ContactUs
-
+export default ContactUs;
