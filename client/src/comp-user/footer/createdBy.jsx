b89252038b68772e1fc6209570dc808a43355e7c
YaliSoltz <yali.soltz11@gmail.com>
2023-02-13 09:05:37 +0200

    seventh commit

--- client/src/comp-user/footer/createdBy.jsx
+++ client/src/comp-user/footer/createdBy.jsx
@@ -4,10 +4,10 @@ const CreatedBy = () => {
   return (
     <div className="created-by">
       <p className="credit-text">אפיון, עיצוב ובניה ע"י</p>
-      <a className="credit-text" href="https://github.com/DanV200" target='_blank'>
+      <a className="credit-text" href="https://www.linkedin.com/in/dan-volper-311855256/" target='_blank'>
         דן וולפר
       </a>
-      <a className="credit-text" href="https://github.com/YaliSoltz" target='_blank'>
+      <a className="credit-text" href="https://www.linkedin.com/in/yalisoltz/" target='_blank'>
         ויה-לי סולץ
       </a>
     </div>
