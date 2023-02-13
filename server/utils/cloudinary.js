6fc49970ec95341ab472cdd42c38cd400e9b8d99
YaliSoltz <yali.soltz11@gmail.com>
2023-02-13 17:21:43 +0200

    delete console logs

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,11 +1,9 @@
-const cloudinary=require('cloudinary').v2;
-require('dotenv').config()
-console.log(process.env.CLOUD_NAME)
-console.log(process.env.CLOUD_KEY)
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 cloudinary.config({
-cloud_name:process.env.CLOUD_NAME,
-api_key:process.env.CLOUD_KEY,
-api_secret:process.env.CLOUD_KEY_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_KEY_SECRET,
 });
 
-module.exports=cloudinary;
+module.exports = cloudinary;
